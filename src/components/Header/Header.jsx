0fe5d7edda6f59ab70fcb35b5c2c970fdf83c214
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-09 00:04:40 +0100

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,55 @@
+import { Link } from "react-router-dom";
+import css from "./Header.module.css";
+import {
+  DIARY_ROUTE,
+  PRODUCT_ROUTE,
+  EXERCISES_ROUTE,
+  PROFILE_ROUTE,
+} from "../../utils/const";
+import LogoPowerPulse from "../../images/svg/PowerPulse.svg";
+import Vectorlogo from "../../images/svg/Vector_logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <Link className={css.logo_wrap} to={DIARY_ROUTE}>
+        <div>
+          <img className={css.logo1} src={Vectorlogo} alt="Logo" />
+        </div>
+        <div>
+          <img className={css.logo2} src={LogoPowerPulse} alt="Logo" />
+        </div>
+      </Link>
+      <nav>
+        <ul className={css.list}>
+          <li className={css.item}>
+            <Link className={css.link} to={DIARY_ROUTE}>
+              Diary
+            </Link>
+          </li>
+          <li>
+            <Link className={css.link} to={PRODUCT_ROUTE}>
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link className={css.link} to={EXERCISES_ROUTE}>
+              Exercises
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={css.list}>
+        <Link>
+          <img className={css.logo1} src={Vectorlogo} alt="user" />
+        </Link>
+        <Link to={PROFILE_ROUTE}>
+          <div className={css.avatart}></div>
+        </Link>
+        <Link className={css.button}>Logout</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
