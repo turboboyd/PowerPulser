0771f2e1a2706939133bb52a1e7351b3cd56439c
Opanasiuk Olexandr <121792730+AlexandrOAV@users.noreply.github.com>
2023-11-10 16:57:55 +0200

    Error-page
    Додав контейнер для бекграунду
    Для сторінки  Нот Фаунд класи свої

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,36 +4,42 @@ import { Link, useLocation } from 'react-router-dom';
 import LogoPowerPulse from "../../images/svg/PowerPulse.svg";
 import Vectorlogo from "../../images/svg/Vector_logo.svg";
 import css from './NotFound.module.css'
+import Container from "../../components/Container/Container";
+
+
 
 const NotFound = () => {
   const location = useLocation();
   const goHomeLink = useRef(location?.state?.from ?? WelcomePage_ROUTE);
 
-  return <div> 
+  return  <Container> 
+   <div className={css.background}>
+    <div className={css.block_error}>
      <Link className={css.logo_wrap} to={WelcomePage_ROUTE}>
         <div>
-          <img className={css.logo1} src={Vectorlogo} alt="Logo" />
+          <img className={css.logo} src={Vectorlogo} alt="Logo" />
         </div>
         <div>
-          <img className={css.logo2} src={LogoPowerPulse} alt="Logo" />
+          <img className={css.logo_text} src={LogoPowerPulse} alt="Logo" />
         </div>
       </Link>
-<h1>404</h1>
-<p>  
+<h1 className={css.title_one}>404</h1>
+<p className={css.text_error}>  
   Sorry, you have reached a page that we could not find. It seems
               that you are lost among the numbers and letters of our virtual
               space. Perhaps this page went on vacation or decided to disappear
               into another dimension. We apologize for this inconvenience.
         </p>
-        <Link to={goHomeLink.current}>
+        <Link className={css.link_goHome} to={goHomeLink.current}>
           Go Home
         </Link>
 {/* 
         <div className={css.backgroundImageWrap}>
       <div className={css.backgroundImage}></div>
     </div> */}
-
-  </div>;
+</div>
+</div>
+  </Container>;
 };
 
 export default NotFound;
