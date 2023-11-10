a3efb428aa02ae43a8e942698ed5f9658c1b6e5e
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-10 16:19:40 +0100

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,6 +43,7 @@ const Header = () => {
                       sprite={sprite}
                       iconId="Gridicons_user"
                     />
+                  
                   </div>
                 </Link>
               </li>
