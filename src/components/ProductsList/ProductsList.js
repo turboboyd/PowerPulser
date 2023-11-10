78974b0d6ce68cd517f11734b35387c46a351962
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-11-10 21:19:35 +0200

    productsList/productsItem

---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,18 @@
+import productsData from '../../RESOURCES/resources/products.json'
+
+import css from './../ExercisesList/ExercisesList.module.css'
+import ProductsItem from '../ProductsItem/ProductsItem'
+
+const ProductsList = () => {
+  const products = productsData.slice(0, 20)
+
+  return (
+    <div className={css.cardContainer}>
+      {products.map((product) => (
+        <ProductsItem key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default ProductsList
