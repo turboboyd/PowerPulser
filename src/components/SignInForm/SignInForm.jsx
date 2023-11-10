b6e42ddfc2a651917a46c7154adac52b6d12cd17
Rammbest <igor-kutsenko@ukr.net>
2023-11-10 14:07:15 +0200

    add message success and error

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useState } from "react";
 
+import sprite from "../../images/svg/sprite.svg";
 import css from "../SignUpForm/SignUpForm.module.css";
 import Button from "../Button/Button";
 
@@ -10,7 +12,6 @@ const passwordRegex =
   /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,32}$/;
 
 const SignInSchema = Yup.object().shape({
-  name: Yup.string().required("Required field"),
   email: Yup.string()
     .matches(emailRegex, "Invalid email format")
     .required("Required field"),
@@ -20,12 +21,19 @@ const SignInSchema = Yup.object().shape({
 });
 
 const SignInForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
   };
   return (
     <>
-      <Formik validationSchema={SignInSchema} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={SignInSchema}
+        onSubmit={handleSubmit}
+      >
         {(formik) => (
           <Form className={css.form} autoComplete="off">
             <div className={css.formWrapper}>
@@ -36,17 +44,74 @@ const SignInForm = () => {
                   name="email"
                   placeholder="Email"
                 />
+                <div className={css.messageWrapper}>
+                  <svg data-status="error" className={css.svgError}>
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="email"
+                    component="div"
+                  />
+                </div>
+
+                {formik.touched.email && !formik.errors.email && (
+                  <div className={css.messageWrapper}>
+                    <svg data-status="success" className={css.svgError}>
+                      <use href={`${sprite}#icon-green`} />
+                    </svg>
+                    <div className={css.successMessage}>Success email</div>
+                  </div>
+                )}
               </div>
 
               <div className={css.fieldWrapper}>
                 <Field
                   className={css.field}
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   placeholder="Password"
                 />
+
+                <div className={css.messageWrapper}>
+                  <svg data-status="error" className={css.svgError}>
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="password"
+                    component="div"
+                  />
+                </div>
+
+                {formik.touched.password && !formik.errors.password && (
+                  <div className={css.messageWrapper}>
+                    <svg data-status="success" className={css.svgError}>
+                      <use href={`${sprite}#icon-green`} />
+                    </svg>
+                    <div className={css.successMessage}>Success password</div>
+                  </div>
+                )}
+                <button
+                  className={css.buttonEye}
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <svg className={css.svgEye}>
+                      <use href={`${sprite}#icon-eye`} />
+                    </svg>
+                  ) : (
+                    <svg className={css.svgEye}>
+                      <use href={`${sprite}#icon-eye-off`} />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
-            <Button type="submit" text="Sign Up" />
+            <Button type="submit" text="Sign In" />
           </Form>
         )}
       </Formik>
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useState } from "react";
 
 import css from "./SignUpForm.module.css";
+import sprite from "../../images/svg/sprite.svg";
 
 import Button from "../Button/Button";
 
@@ -22,12 +24,20 @@ const SignUpSchema = Yup.object().shape({
 });
 
 const SignUpForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
   };
+
   return (
     <>
-      <Formik validationSchema={SignUpSchema} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        validationSchema={SignUpSchema}
+        onSubmit={handleSubmit}
+      >
         {(formik) => (
           <Form className={css.form} autoComplete="off">
             <div className={css.formWrapper}>
@@ -38,6 +48,27 @@ const SignUpForm = () => {
                   name="name"
                   placeholder="Name"
                 />
+
+                <div className={css.messageWrapper}>
+                  <svg data-status="error" className={css.svgError}>
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="name"
+                    component="div"
+                  />
+                </div>
+
+                {formik.touched.name && !formik.errors.name && (
+                  <div className={css.messageWrapper}>
+                    <svg data-status="success" className={css.svgError}>
+                      <use href={`${sprite}#icon-green`} />
+                    </svg>
+                    <div className={css.successMessage}>Success name</div>
+                  </div>
+                )}
               </div>
 
               <div className={css.fieldWrapper}>
@@ -47,14 +78,72 @@ const SignUpForm = () => {
                   name="email"
                   placeholder="Email"
                 />
+
+                <div className={css.messageWrapper}>
+                  <svg data-status="error" className={css.svgError}>
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="email"
+                    component="div"
+                  />
+                </div>
+
+                {formik.touched.email && !formik.errors.email && (
+                  <div className={css.messageWrapper}>
+                    <svg data-status="success" className={css.svgError}>
+                      <use href={`${sprite}#icon-green`} />
+                    </svg>
+                    <div className={css.successMessage}>Success email</div>
+                  </div>
+                )}
               </div>
 
               <div className={css.fieldWrapper}>
                 <Field
                   className={css.field}
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   placeholder="Password"
                 />
+
+                <div className={css.messageWrapper}>
+                  <svg data-status="error" className={css.svgError}>
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="password"
+                    component="div"
+                  />
+                </div>
+
+                {formik.touched.password && !formik.errors.password && (
+                  <div className={css.messageWrapper}>
+                    <svg data-status="success" className={css.svgError}>
+                      <use href={`${sprite}#icon-green`} />
+                    </svg>
+                    <div className={css.successMessage}>Success password</div>
+                  </div>
+                )}
+                <button
+                  className={css.buttonEye}
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <svg className={css.svgEye}>
+                      <use href={`${sprite}#icon-eye`} />
+                    </svg>
+                  ) : (
+                    <svg className={css.svgEye}>
+                      <use href={`${sprite}#icon-eye-off`} />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
             <Button type="submit" text="Sign Up" />
