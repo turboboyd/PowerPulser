4e92fb3adfe83db64bfc31bdeeae1b836d25547b
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-11 20:54:17 +0100

    add

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -6,33 +6,32 @@ import Logo from "../../components/Logo/Logo";
 import BgImageWelcomePage from "../../components/BgImageWelcomePage/BgImageWelcomePage";
 import css from "./WelcomPage.module.css";
 import Container from "../../components/Container/Container";
+import Icon from "../../components/ComponIcon/Icon";
+import Section from "../../components/Container/Section";
 
 const WelcomePage = () => {
   return (
-    <section className={css.section}>
+    <Section>
       <Container>
-        {/* <div className={css.wrapper}> */}
-        <Logo />{" "}
-        {/* <h1 className={css.title}>
-          Transforming your body shape with Power Pulse
-        </h1> */}
-        {/* <svg className={css.svg}>
-            <use href={`${sprite}#icon-line`} />
-          </svg> */}
-        {/* <div className={css.buttonWrapper}>
+        <div className={css.wrapper}>
+          <Logo />{" "}
+          <h1 className={css.title}>
+            Transforming your body shape with Power Pulse
+          </h1>
+          <Icon className={css.exerciseArrowSvg} iconId="" />
+          <div className={css.buttonWrapper}>
             <Link className={css.link} to="/signup">
               <Button type="submit" text="Sign Up" />
             </Link>
             <Link to="/signin">
               <Button type="submit" text="Sign In" />
             </Link>
-          </div> */}
-        {/* </div> */}
+          </div>
+        </div>
       </Container>
 
-      <BgImageWelcomePage/>
-
-    </section>
+      <BgImageWelcomePage />
+    </Section>
   );
 };
 
