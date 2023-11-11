7e2ffd9234acea8e750d28dcac105731ca3c38b7
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-11 15:53:15 +0100

    add

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,36 +3,35 @@ import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Logo from "../../components/Logo/Logo";
 import sprite from "../../images/svg/sprite.svg";
-import BgImageWelcomePage from "../../components/BgImageWelcomePage/BgImageWelcomePage";
+// import BgImageWelcomePage from "../../components/BgImageWelcomePage/BgImageWelcomePage";
 import css from "./WelcomPage.module.css";
+import Container from "../../components/Container/Container";
 
 const WelcomePage = () => {
   return (
-    <>
-      <div>
-        <section className={css.section}>
-          <div className={css.wrapper}>
-            <Logo />{" "}
-            <h1 className={css.title}>
-              Transforming your body shape with Power Pulse
-            </h1>
-            <svg className={css.svg}>
-              <use href={`${sprite}#icon-line`} />
-            </svg>
-            <div className={css.buttonWrapper}>
-              <Link className={css.link} to="/signup">
-                <Button type="submit" text="Sign Up" />
-              </Link>
-              <Link to="/signin">
-                <Button type="submit" text="Sign In" />
-              </Link>
-            </div>
-          </div>
+    <section className={css.section}>
+      <Container>
+        <div className={css.wrapper}>
+          <Logo />{" "}
+          <h1 className={css.title}>
+            Transforming your body shape with Power Pulse
+          </h1>
+          {/* <svg className={css.svg}>
+            <use href={`${sprite}#icon-line`} />
+          </svg> */}
+          {/* <div className={css.buttonWrapper}>
+            <Link className={css.link} to="/signup">
+              <Button type="submit" text="Sign Up" />
+            </Link>
+            <Link to="/signin">
+              <Button type="submit" text="Sign In" />
+            </Link>
+          </div> */}
+        </div>
+      </Container>
 
-          <BgImageWelcomePage></BgImageWelcomePage>
-        </section>
-      </div>
-    </>
+      {/* <BgImageWelcomePage></BgImageWelcomePage> */}
+    </section>
   );
 };
 
