3d9458736c2ae1dec7c14e44ac771bda3eb1adae
Olena-Rudnieva <rudnievao@gmail.com>
2023-11-13 17:39:30 +0200

    Add exercise modal

--- src/components/ExercisesItem/ExercisesItem.js
+++ src/components/ExercisesItem/ExercisesItem.js
@@ -1,16 +1,53 @@
-import css from './ExercisesItem.module.css'
-import Icon from '../ComponIcon/Icon'
+import css from './ExercisesItem.module.css';
+import Icon from '../ComponIcon/Icon';
+import { useState } from 'react';
+import BasicModalWindow from '../ModalWindow/BasicModalWindow/BasicModalWindow';
+import AddExerciseForm from '../ModalWindow/AddExerciseForm/AddExerciseForm';
+import AddExerciseSuccess from '../ModalWindow/AddExerciseSuccess/AddExerciseSuccess';
 
 const ExercisesItem = ({ exercise }) => {
+  const [modalExercise, setModalExercise] = useState(false);
+  const [modalSuccess, setModalSuccess] = useState(false);
+  const [selectedExercise, setSelectedExercise] = useState('');
+
+  const handleModalExercise = () => {
+    setModalExercise((state) => !state);
+  };
+
+  const handleModalSuccess = () => {
+    setModalSuccess((state) => !state);
+  };
+
+  const handleSelectedExercise = (data) => {
+    setSelectedExercise(data);
+  };
   return (
     <div className={css.exerciseWrapper}>
       <div className={css.exerciseCardTopPart}>
         <p className={css.exerciseCardTopDiet}>workout</p>
-        <button className={css.exerciseArrow}>
+        <button className={css.exerciseArrow} onClick={handleModalExercise}>
           Start
           <Icon className={css.exerciseArrowSvg} iconId="Arrow" />
         </button>
       </div>
+      {modalExercise && (
+        <BasicModalWindow handleModalToggle={handleModalExercise}>
+          <AddExerciseForm
+            handleModalExercise={handleModalExercise}
+            handleModalSuccess={handleModalSuccess}
+            exercise={exercise}
+            handleSelectedExercise={handleSelectedExercise}
+          />
+        </BasicModalWindow>
+      )}
+      {modalSuccess && (
+        <BasicModalWindow handleModalToggle={handleModalSuccess}>
+          <AddExerciseSuccess
+            handleModalSuccess={handleModalSuccess}
+            selectedExercise={selectedExercise}
+          />
+        </BasicModalWindow>
+      )}
       <div className={css.exerciseName}>
         <Icon className={css.exerciseManSvg} iconId="Runner" />
         <h3 className={css.exerciseNameText}>{exercise.name}</h3>
@@ -30,6 +67,6 @@ const ExercisesItem = ({ exercise }) => {
         </li>
       </ul>
     </div>
-  )
-}
-export default ExercisesItem
+  );
+};
+export default ExercisesItem;
