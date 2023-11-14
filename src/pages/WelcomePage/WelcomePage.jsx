121ac34dcf61b464b24b78edff280291cdb5af71
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-11-14 23:00:16 +0100

    done

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -13,56 +13,67 @@ import Icon from "../../components/ComponIcon/Icon";
 const WelcomePage = () => {
   return (
     <Container>
-      <div className={css.logo}>
-        <Logo />{' '}
-      </div>
-      <div className={css.wrapper}>
-        <h1 className={css.title}>
-          Transforming your <span className={css.bodyText}>body</span> shape
-          with Power Pulse
-        </h1>
-        <div className={css.icon}>
-          <Icon className={css.exerciseArrowSvg} iconId="icon-line" />
+        <div className={css.logo}>
+          <Logo />{' '}
         </div>
-        <div className={css.buttonWrapper}>
-          <Link className={css.link} to="/signup">
-            <Button type="submit" className={css.buttonSignUp} text="Sign Up" />
-          </Link>
-          <Link to="/signin">
-            <Button type="submit" className={css.buttonSignIn} text="Sign In" />
-          </Link>
-        </div>
-        <div className={css.polygon}>
-          <ButtonInfo
-            type="button"
-            buttonClassName={css.buttonPlay}
-            iconClassName={css.iconPlay}
-            text={
-              <div className={css.textPlay}>
-                <span className={css.boldTextPlay}>350+</span>{' '}
-                <span className={css.smallTextPlay}>Video Tutorial</span>
-              </div>
-            }
-            sprite={sprite}
-            iconId="Polygon" //
-          />
+        <div className={css.wrapper}>
+          <h1 className={css.title}>
+            Transforming your <span className={css.bodyText}>body</span> shape
+            with Power Pulse
+          </h1>
+          <div className={css.icon}>
+            <Icon className={css.exerciseArrowSvg} iconId="icon-line" />
+          </div>
+          <div className={css.buttonWrapper}>
+            <Link className={css.link} to="/signup">
+              <Button
+                type="submit"
+                className={css.buttonSignUp}
+                text="Sign Up"
+              />
+            </Link>
+            <Link to="/signin">
+              <Button
+                type="submit"
+                className={css.buttonSignIn}
+                text="Sign In"
+              />
+            </Link>
+          </div>
+
+          <div className={css.polygon}>
+            <ButtonInfo
+              type="button"
+              buttonClassName={css.buttonPlay}
+              iconClassName={css.iconPlay}
+              text={
+                <div className={css.textPlay}>
+                  <span className={css.boldTextPlay}>350+</span>{' '}
+                  <span className={css.smallTextPlay}>Video Tutorial</span>
+                </div>
+              }
+              sprite={sprite}
+              iconId="Polygon"
+            />
+          </div>
+
+          <div className={css.runner}>
+            <ButtonInfo
+              type="button"
+              buttonClassName={css.calButton}
+              iconClassName={css.calIcon}
+              text={
+                <div className={css.textSvg}>
+                  <span className={css.boldText}>500</span>{' '}
+                  <span className={css.smallText}>cal</span>
+                </div>
+              }
+              sprite={sprite}
+              iconId="Runner"
+            />
+          </div>
         </div>
 
-        <ButtonInfo
-          className={css.buttonA}
-          type="button"
-          buttonClassName={css.calButton}
-          iconClassName={css.calIcon}
-          text={
-            <div className={css.textSvg}>
-              <span className={css.boldText}>500</span>{' '}
-              <span className={css.smallText}>cal</span>
-            </div>
-          }
-          sprite={sprite}
-          iconId="Runner" // Replace with the actual ID from your sprite
-        />
-      </div>
       <BackgroundImage />
     </Container>
   );
