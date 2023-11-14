7caa9c6e5d8a7b79ce1b288eb1d04f99bf627a2c
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-14 23:48:30 +0100

    add

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,33 @@
-import TitlePage from "../../components/TitlePage/TitlePage";
+import { Link } from "react-router-dom";
 
-const AuthPage = () => {
-  return <TitlePage title="AuthPage" />;
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import css from "./AuthPage.module.css";
+import BackgroundImage from "../../components/BackgroundImage/BackgroundImage";
+
+const SignUpPage = () => {
+
+
+  const AuthPage =
+    " Thank you for your interest in our platform. To complete the registration process, please provide us with the following information.";
+  return (
+    <>
+      <h2 className={css.title}>Sign Up</h2>
+      <p className={css.text}>
+        Thank you for your interest in our platform. To complete the
+        registration process, please provide us with the following information.
+      </p>
+      <SignUpForm />
+      <div className={css.textWrapper}>
+        <p className={css.textHint}>Already have account?</p>
+        <Link className={css.link} to="/signin">
+          Sign In
+        </Link>
+      </div>
+      <BackgroundImage/>
+    </>
+  );
 };
 
 export default AuthPage;
+
+
