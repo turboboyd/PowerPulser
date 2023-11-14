2705d78d413f226c12c3b268698b0e1ab03218b4
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-11-14 07:15:52 +0200

    products-background-image

---
+++ src/components/BackgroundImage/Products/ProductsBackgroundImage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from './ProductsBackgroundImage.module.css'
+
+const ProductsBackgroundImage = () => {
+  return <div className={css.productsBgImg}></div>
+}
+
+export default ProductsBackgroundImage
