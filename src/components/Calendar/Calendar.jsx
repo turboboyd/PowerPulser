c5d3653c6ccf0d066ac03529e1c28768616d8e2e
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-11-14 22:59:29 +0100

    add calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,39 +1,16 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import styled from "styled-components";
-import DayDashboard from '../DayDashboard/DayDashboard';
-import DayExercises from '../DayExercises/DayExercises';
-import DayProducts from '../DayProducts/DayProducts';
-import CalendarIconPNG from '../../images/calendar.png';
+import CalendarStyles from "./Calendar.module.css";
+import DayDashboard from "../DayDashboard/DayDashboard";
+import DayExercises from "../DayExercises/DayExercises";
+import DayProducts from "../DayProducts/DayProducts";
+import calendarIcon from "../../images/calendar.png";
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 10px;
-`;
-
-const CalendarIcon = styled.span`
-  cursor: pointer;
-  color: orange;
-  font-size: 1.2em;
-  margin-right: 10px;
-  background: url(${CalendarIconPNG}) no-repeat;
-  background-size: contain;
-  width: 20px;
-  height: 20px;
-`;
-
-const StyledDatePicker = styled(DatePicker)`
-  border: 1px solid #efa082;
-  border-radius: 4px;
-  padding: 5px;
-`;
-
-const ParentComponent = ({ user }) => {
+const ParentComponent = ({ user, registrationDate }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [calendarOpen, setCalendarOpen] = useState(false);
-  const userRegistrationDate = new Date(user.registrationDate);
+  const userRegistrationDate = new Date(registrationDate);
 
   const handlePrevDay = () => {
     const prevDate = new Date(selectedDate);
@@ -42,41 +19,62 @@ const ParentComponent = ({ user }) => {
   };
 
   const handleNextDay = () => {
-  const nextDate = new Date(selectedDate);
-  nextDate.setDate(nextDate.getDate() + 1);
-  if (nextDate <= new Date()) {
-    setSelectedDate(nextDate);
-  }
-};
+    const nextDate = new Date(selectedDate);
+    nextDate.setDate(nextDate.getDate() + 1);
+    if (nextDate <= new Date()) {
+      setSelectedDate(nextDate);
+    }
+  };
 
   const toggleCalendar = () => {
-    console.log("Calendar is toggled");
     setCalendarOpen(!calendarOpen);
+  };
+
+  const handleCalendarChange = (date) => {
+    setSelectedDate(date);
+    toggleCalendar();
   };
 
   return (
     <div>
-      <div onClick={toggleCalendar}>
-        <span>{selectedDate.toLocaleDateString("en-GB")}</span>
-      </div>
-      {calendarOpen && (
+      <div className={CalendarStyles.container}>
+          <img
+          src={calendarIcon}
+          alt="calendar icon"
+          className={CalendarStyles.calendarIcon}
+          onClick={toggleCalendar}
+        />
+        <DatePicker
+          showYearDropdown
+          dateFormat="dd/MM/yyyy"
+          selected={selectedDate}
+          onChange={handleCalendarChange}
+          minDate={userRegistrationDate}
+          maxDate={new Date()}          
+          className={CalendarStyles.datePicker}
+
+        />
         <div>
-          <StyledDatePicker
-            selected={selectedDate}
-            onChange={(date) => {
-              setSelectedDate(date);
-              toggleCalendar();
-            }}
-            minDate={userRegistrationDate}
-            maxDate={new Date()} 
-          />
+          <span
+            className={`${CalendarStyles.arrowSpan} ${CalendarStyles.prevArrow}`}
+            onClick={handlePrevDay}
+          >
+            {"<"}
+          </span>
+          <span
+            className={`${CalendarStyles.arrowSpan} ${CalendarStyles.nextArrow}`}
+            onClick={handleNextDay}
+          >
+            {">"}
+          </span>
         </div>
-      )}
-      <div>
-        <CalendarIcon onClick={handlePrevDay}>{"<"}</CalendarIcon>
-        <CalendarIcon onClick={handleNextDay}>{">"}</CalendarIcon>
       </div>
+
+      <DayDashboard selectedDate={selectedDate} />
+      <DayExercises selectedDate={selectedDate} />
+      <DayProducts selectedDate={selectedDate} />
     </div>
   );
-}
+};
+
 export default ParentComponent;
