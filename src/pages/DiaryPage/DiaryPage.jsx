3fc83c56861be988d585e0776029bbef319bedbf
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-11-15 19:07:07 +0100

    css

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,8 +1,6 @@
 import css from "./DiaryPage.module.css";
 
-import DayExercises from "../../components/DayExercises/DayExercises";
-import DayProducts from "../../components/DayProducts/DayProducts";
-import DayDashbroad from "../../components/DayDashboard/DayDashboard";
+
 import TitlePage from "../../components/TitlePage/TitlePage";
 import ParentComponent from "../../components/Calendar/Calendar";
 
@@ -10,13 +8,9 @@ const DiaryPage = () => {
   return (
     <div className={css.diaryPage}>
       <TitlePage title="Diary" />
-      <ParentComponent />
+      
       <div className={css.sectionWrap}>
-        <DayDashbroad />
-        <div>
-          <DayProducts />
-          <DayExercises />
-        </div>
+        <ParentComponent />
       </div>
     </div>
   );
