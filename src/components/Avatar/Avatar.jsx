f7ee483db9451a4fe46ccf8145b1a7609bce7432
Rammbest <igor-kutsenko@ukr.net>
2023-11-16 14:19:29 +0200

    updete

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,32 +1,55 @@
+import { useState } from "react";
+
 import Icon from "../ComponIcon/Icon";
 import css from "./Avatar.module.css";
 
 import { selectUser } from "../../redux/auth/authSelectors";
+import { selectAvatar } from "../../redux/avatar/avatarSelectors";
 import { useSelector, useDispatch } from "react-redux";
-// import { updateAvatar } from "../../redux/auth/authOperation";
+import { uploadAvatar } from "../../redux/avatar/avatarOperations";
 
 const Avatar = () => {
   const user = useSelector(selectUser);
+  const avatar = useSelector(selectAvatar);
+  const [avatarUser, setAvatarUser] = useState(avatar);
   const dispatch = useDispatch();
 
-  const handleChangeAvatar = (e) => {
-    let formData = new FormData();
-    const file = e.target.files[0];
+  const userAvatar = (
+    <img
+      src={avatarUser}
+      width="100%"
+      alt="Avatar"
+      style={{ borderRadius: "100%" }}
+    />
+  );
+  const avatarLogo = <Icon className={css.iconAvatar} iconId={"icon-avatar"} />;
 
+  const handleChangeAvatar = (e) => {
+    // let formData = new FormData();
+    // const file = e.target.files[0];
+
+    // formData.append("avatar", file);
+    // setAvatarUser(file);
+    const file = e.target.files[0];
+    if (file) {
+      const blob = new Blob([file]);
+      const objectURL = URL.createObjectURL(blob);
+      setAvatarUser(objectURL);
+    }
+
+    let formData = new FormData();
     formData.append("avatar", file);
 
     console.log("Selected File:", file);
     console.log("FormData:", [...formData]);
 
-    // dispatch(updateAvatar(formData));
+    dispatch(uploadAvatar(formData));
   };
 
   return (
     <>
       <div className={css.avatarWrapper}>
-        <div className={css.avatar}>
-          <Icon className={css.iconAvatar} iconId={"icon-avatar"} />
-        </div>
+        <div className={css.avatar}>{avatar ? avatarLogo : userAvatar}</div>
         <form className={css.form}>
           <input
             type="file"
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,21 @@
+import { Container, Section } from "../../components/Container";
 import TitlePage from "../../components/TitlePage/TitlePage";
 import UseForm from "../../components/UserForm/UserForm";
 import UserCard from "../../components/UserCard/UserCard";
+
 import css from "./ProfilePage.module.css";
 
 const ProfilePage = () => {
   return (
-    <>
-      <TitlePage title="Profile Settings" className={css.title} />
-      <div className={css.container}>
-        <UseForm />
-        <UserCard />
-      </div>
-    </>
+    <Section className={css.section}>
+      <Container>
+        <TitlePage title="Profile Settings" className={css.title} />
+        <div className={css.container}>
+          <UseForm />
+          <UserCard />
+        </div>
+      </Container>
+    </Section>
   );
 };
 
