25ef1b3d27f7a36faaed159f00076659918ba888
arhistratig65 <118848591+arhistratig65@users.noreply.github.com>
2023-11-18 22:06:07 +0200

    fix

--- src/components/ProductsFilter/ProductsFilter.jsx
+++ src/components/ProductsFilter/ProductsFilter.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Select from 'react-select';
 import sprite from '../../images/svg/InlineSprite.svg';
-import { setFilter } from '../../redux/products/productsSlice';
 import styles from './ProductsFilter.module.css';
 import { Formik, Form, Field } from 'formik';
-import customStyles from './ProductFilterSelectStyles'
-import capitalizedWord from '../../utils/capitalizedWord'
+import customStyles from './ProductFilterSelectStyles';
+import capitalizedWord from '../../utils/capitalizedWord';
+import selectCategoriesProducts from '../../redux/products/productsSelectors';
+import { fetchProducts } from '../../redux/products/productsOperations';
 
 // Опции для селекторов
-const optionsRec = [
+const options = [
   { value: 'all', label: 'All' },
-  { value: true, label: 'Recommended' },
-  { value: false, label: 'Not recommended' },
+  { value: 'true', label: 'Recommended ' },
+  { value: 'false', label: 'Not recommended' },
 ];
+
+// Категории продуктов
+// const categories = useSelector(selectCategoriesProducts);
+
+
 
 // Категории продуктов
 const categories = [
@@ -39,134 +45,158 @@ const categories = [
 ];
 
 
+
 // Преобразование категорий для селектора
-const categoriesList = categories?.map((el) => ({
-  value: el,
-  label: capitalizedWord(el),
-}));
 
 
 // Компонент ProductsFilter
 const ProductsFilter = () => {
+    // Получение диспетчера Redux
   const dispatch = useDispatch();
+
+    // Состояния для хранения значения поиска, выбранной категории и выбранной рекомендации
   const [search, setSearch] = useState('');
   const [category, setCategory] = useState('');
-  const [recommended, setRecommended] = useState(optionsRec[0]);
+  const [recommended, setRecommended] = useState(options[0]);
 
-  
+  // const categories = useSelector(selectCategoriesProducts);
+
+ // Эффект, который отправляет запрос на получение продуктов при изменении рекомендации, категории или поискового запроса
+  useEffect(() => {
+    dispatch(
+      fetchProducts({
+        recommended: recommended.value,
+        category: category.value,
+        search,
+      }),
+    );
+  }, [recommended, category, search, dispatch]);
+
+ // Формирование списка категорий для селектора
+ const categoriesList = categories?.map((el) => ({
+  value: el,
+  label: capitalizedWord(el),
+}));
+
+  // Обработчик отправки формы поиска
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const searchValue = e.target.elements[0].value;
+  setSearch(searchValue);
+};
 
 
-  return (
-    <Formik
-  initialValues={{
-    search: '',
-    category: categoriesList[0],
-    recommended: optionsRec[0],
-  }}
-  onSubmit={(values) => {
-    // Обработка отправки формы
-  }}
+return (
+  <Formik
+initialValues={{
+  search: '',
+  category: categoriesList[0],
+  recommended: options[0],
+}}
+onSubmit={(values) => {
+  // Обработка отправки формы
+}}
 >
-  {(formik) => (
-    <Form>
-      <ul className={styles['products-filter-list']}>
-        {/* Поиск */}
-        <li>
-          <label className={styles['products-filter-label']}>
-            <Field
-              name="search"
-              render={({ field }) => (
-                <input
-                  {...field}
-                  type="text"
-                  placeholder="Search"
-                  className={styles['products-filter-search']}
-                  value={formik.values.search}
-                  onChange={(event) => formik.handleChange(event)}
-                />
-              )}
-            />
-            {/* Кнопка для закрытия поиска */}
-            {formik.values.search.length > 0 && (
-              <button
-                className={styles['products-btn-clouse']}
-                onClick={() => formik.setFieldValue('search', '')}
-                type="button"
-              >
-                <svg className={styles['products-svg-clouse']}>
-                  <use href={`${sprite}#Close`}></use>
-                </svg>
-              </button>
+{(formik) => (
+  <Form onSubmit={handleSubmit}>
+    <ul className={styles['products-filter-list']}>
+      {/* Поиск */}
+      <li>
+        <label className={styles['products-filter-label']}>
+          <Field
+            name="search"
+            render={({ field }) => (
+              <input
+                {...field}
+                type="text"
+                placeholder="Search"
+                className={styles['products-filter-search']}
+                value={formik.values.search}
+                onChange={(event) => formik.handleChange(event)}
+              />
             )}
-            {/* Кнопка для запуска поиска */}
+          />
+          {/* Кнопка для закрытия поиска */}
+          {formik.values.search.length > 0 && (
             <button
-              className={styles['products-btn-search']}
-              type="submit"
+              className={styles['products-btn-clouse']}
+              onClick={() => formik.setFieldValue('search', '')}
+              type="button"
             >
-              <svg className={styles['products-svg-search']}>
-                <use href={`${sprite}#Search`}></use>
+              <svg className={styles['products-svg-clouse']}>
+                <use href={`${sprite}#Close`}></use>
               </svg>
             </button>
-          </label>
-        </li>
+          )}
+          {/* Кнопка для запуска поиска */}
+          <button
+            className={styles['products-btn-search']}
+            type="submit"
+          >
+            <svg className={styles['products-svg-search']}>
+              <use href={`${sprite}#Search`}></use>
+            </svg>
+          </button>
+        </label>
+      </li>
 
-        {/* Выбор категории */}
-        <li>
-          <div className={styles['select-wrapper']}>
-            <Select
-              className={styles['select-wrapper']}
-              styles={customStyles}
-              value={formik.values.category}
-              onChange={(event) => formik.setFieldValue('category', event)}
-              placeholder="Categories"
-              options={categoriesList || []}
-              theme={(theme) => ({
-                ...theme,
-                colors: {
-                  ...theme.colors,
-                  primary50: 'rgba(255, 255, 255, 0.10)',
-                  primary: 'transparent',
-                  neutral40: '#EFEDE8',
-                  neutral20: 'transparent',
-                  neutral30: 'transparent',
-                  neutral50: 'rgba(239, 237, 232, 1)',
-                  neutral80: 'rgba(239, 237, 232, 1)',
-                },
-              })}
-            />
-          </div>
-        </li>
+      {/* Выбор категории */}
+      <li>
+        <div className={styles['select-wrapper']}>
+          <Select
+            className={styles['select-wrapper']}
+            styles={customStyles}
+            value={formik.values.category}
+            onChange={(event) => formik.setFieldValue('category', event)}
+            placeholder="Categories"
+            options={categoriesList || []}
+            theme={(theme) => ({
+              ...theme,
+              colors: {
+                ...theme.colors,
+                primary50: 'rgba(255, 255, 255, 0.10)',
+                primary: 'transparent',
+                neutral40: '#EFEDE8',
+                neutral20: 'transparent',
+                neutral30: 'transparent',
+                neutral50: 'rgba(239, 237, 232, 1)',
+                neutral80: 'rgba(239, 237, 232, 1)',
+              },
+            })}
+          />
+        </div>
+      </li>
 
-        {/* Выбор рекомендаций */}
-        <li>
-          <div className={styles['select-wrapper']}>
-            <Select
-              className={styles['select-wrapper']}
-              onChange={(event) => formik.setFieldValue('recommended', event)}
-              styles={customStyles}
-              value={formik.values.recommended}
-              theme={(theme) => ({
-                ...theme,
-                colors: {
-                  ...theme.colors,
-                  primary50: 'rgba(255, 255, 255, 0.10)',
-                  primary: 'transparent',
-                  neutral40: '#EFEDE8',
-                  neutral20: 'transparent',
-                  neutral30: 'transparent',
-                  neutral50: 'rgba(239, 237, 232, 1)',
-                  neutral80: 'rgba(239, 237, 232, 1)',
-                },
-              })}
-              options={optionsRec}
-            />
-          </div>
-        </li>
-      </ul>
-    </Form>
-  )}
+      {/* Выбор рекомендаций */}
+      <li>
+        <div className={styles['select-wrapper']}>
+          <Select
+            className={styles['select-wrapper']}
+            onChange={(event) => formik.setFieldValue('recommended', event)}
+            styles={customStyles}
+            value={formik.values.recommended}
+            theme={(theme) => ({
+              ...theme,
+              colors: {
+                ...theme.colors,
+                primary50: 'rgba(255, 255, 255, 0.10)',
+                primary: 'transparent',
+                neutral40: '#EFEDE8',
+                neutral20: 'transparent',
+                neutral30: 'transparent',
+                neutral50: 'rgba(239, 237, 232, 1)',
+                neutral80: 'rgba(239, 237, 232, 1)',
+              },
+            })}
+            options={options}
+          />
+        </div>
+      </li>
+    </ul>
+  </Form>
+)}
 </Formik>
-  );
+);
 };
 
 export default ProductsFilter;
