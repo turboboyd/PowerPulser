c0accad4922d4cd209e230d168b74cc41c8df53d
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-18 23:08:19 +0100

    add

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,3 @@
-import { Container, Section } from "../../components/Container";
 import TitlePage from "../../components/TitlePage/TitlePage";
 import UseForm from "../../components/UserForm/UserForm";
 import UserCard from "../../components/UserCard/UserCard";
@@ -6,18 +5,13 @@ import UserCard from "../../components/UserCard/UserCard";
 import css from "./ProfilePage.module.css";
 
 const ProfilePage = () => {
-
   return (
     <>
-      {/* <Section className={css.section}>
-        <Container> */}
-          <TitlePage title="Profile Settings" className={css.title} />
-          <div className={css.container}>
-            <UseForm />
-            <UserCard />
-          </div>
-        {/* </Container>
-      </Section> */}
+      <TitlePage title="Profile Settings" className={css.title} />
+      <div className={css.container}>
+        <UseForm />
+        <UserCard />
+      </div>
     </>
   );
 };
