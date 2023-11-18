372a3bbfd7aa9022751131b7fad515915e483cd9
arhistratig65 <118848591+arhistratig65@users.noreply.github.com>
2023-11-18 13:43:57 +0200

    переделал через Formik, и вынес стили  отдельно

--- src/components/ProductsFilter/ProductsFilter.jsx
+++ src/components/ProductsFilter/ProductsFilter.jsx
@@ -4,6 +4,8 @@ import Select from 'react-select';
 import sprite from '../../images/svg/InlineSprite.svg';
 import { setFilter } from '../../redux/products/productsSlice';
 import styles from './ProductsFilter.module.css';
+import { Formik, Form, Field } from 'formik';
+import customStyles from './ProductFilterSelectStyles'
 
 // Опции для селекторов
 const optionsRec = [
@@ -48,66 +50,8 @@ const categoriesList = categories?.map((el) => ({
 }));
 
 // Стили для компонентов Select
-const customStyles = {
-  control: (provided) => ({
-    ...provided,
-    backgroundColor: 'transparent', // Стилизация фона окна
-    WebkitAppearance: 'none',
-    MozAppearance: 'none',
-    margin: '7px',
-  }),
-  option: (provided, { isFocused, isSelected }) => ({
-    ...provided,
-    backgroundColor: isSelected
-      ? 'rgba(28, 28, 28, 1)'
-      : isFocused
-      ? 'rgba(28, 28, 28, 1)'
-      : 'rgba(28, 28, 28, 1)', // Стилизация фона активной опции и ховера
-    color: isSelected ? '#E6533C' : '#EFEDE8', // Стилизация цвета текста активной опции в списке
-    padding: '14px',
-  }),
-  menu: (provided) => ({
-    ...provided,
-    backgroundColor: 'rgba(28, 28, 28, 1)', // фон для списка
-  }),
-  singleValue: (provided) => ({
-    ...provided,
-    color: '#EFEDE8', // Цвет текста активного селектора в окне
-  }),
-  indicatorSeparator: (provided) => ({
-    ...provided,
-    backgroundColor: 'transparent', // Цвет разделителя
-  }),
-  dropdownIndicator: (provided) => ({
-    ...provided,
-    color: '#EFEDE8',
-  }),
-  container: (provided) => ({
-    ...provided,
-    border: '1px solid rgba(239, 237, 232, 0.30)',
-    borderRadius: '12px',
-    outline: 'none',
-  }),
-  menuList: (base) => ({
-    ...base,
-    borderRadius: '12px', // Бордер при скроле
-    '::-webkit-scrollbar': {
-      width: '8px', // Ширина полосы прокрутки
-    },
-    '::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(239, 237, 232, 0.1)', // Цвет полосы прокрутки
-      borderRadius: '12px', // Скругление углов полосы прокрутки
-      height: '147px', // Высота полосы прокрутки
-      // Отступ справа
-  
-    },
-    '::-webkit-scrollbar-track': {
-      backgroundColor: 'rgba(28, 28, 28, 1)', // Цвет фона полосы прокрутки
-      borderRadius: '12px', // Скругление углов фона полосы прокрутки
-    },
-    overflowY: 'scroll',
-  }),
-};
+
+
 
 // Компонент ProductsFilter
 const ProductsFilter = () => {
@@ -117,9 +61,10 @@ const ProductsFilter = () => {
   const [recommended, setRecommended] = useState(optionsRec[0]);
 
   // Обработчик изменения ввода для поиска
-  const onChangeSearch = (event) => {
+  const onChangeSearch = (event, setFieldValue) => {
     const text = event.target.value;
     setSearch(text);
+    setFieldValue('search', text);
     dispatch(
       setFilter({
         search: text,
@@ -132,8 +77,9 @@ const ProductsFilter = () => {
   };
 
   // Обработчик изменения категории
-  const onCategoriesChange = (event) => {
+  const onCategoriesChange = (event, setFieldValue) => {
     setCategory(event);
+    setFieldValue('category', event.value);
     dispatch(
       setFilter({
         category: event.value,
@@ -144,8 +90,9 @@ const ProductsFilter = () => {
   };
 
   // Обработчик изменения рекомендаций
-  const onRecommendedChange = (event) => {
+  const onRecommendedChange = (event, setFieldValue) => {
     setRecommended(event);
+    setFieldValue('recommended', event.value);
     dispatch(
       setFilter({
         recommended: event.value,
@@ -156,8 +103,9 @@ const ProductsFilter = () => {
   };
 
   // Обработчик удаления текста из поля поиска
-  const delTextInput = () => {
+  const delTextInput = (setFieldValue) => {
     setSearch('');
+    setFieldValue('search', '');
     dispatch(
       setFilter({
         search: '',
@@ -168,94 +116,118 @@ const ProductsFilter = () => {
   };
 
   return (
-    <ul className={styles['products-filter-list']}>
-      {/* Поиск */}
-      <li>
-        <label className={styles['products-filter-label']}>
-          <input
-            value={search}
-            onChange={onChangeSearch}
-            name="productSearch"
-            type="text"
-            placeholder="Search"
-            className={styles['products-filter-search']}
-          />
-          {/* Кнопка для закрытия поиска */}
-          {search.length > 0 && (
-            <button
-              className={styles['products-btn-clouse']}
-              onClick={delTextInput}
-              type="button"
-            >
-              <svg className={styles['products-svg-clouse']}>
-                <use href={`${sprite}#Close`}></use>
-              </svg>
-            </button>
-          )}
-          {/* Кнопка для запуска поиска */}
-          <button className={styles['products-btn-search']} type="button">
-            <svg className={styles['products-svg-search']}>
-              <use href={`${sprite}#Search`}></use>
-            </svg>
-          </button>
-        </label>
-      </li>
+    <Formik
+      initialValues={{
+        search: '',
+        category: categoriesList[0],
+        recommended: optionsRec[0],
+      }}
+      onSubmit={(values) => {
+        // Обработка отправки формы
+      }}
+    >
+      {({ values, setFieldValue }) => (
+        <Form>
+          <ul className={styles['products-filter-list']}>
+            {/* Поиск */}
+            <li>
+              <label className={styles['products-filter-label']}>
+                <Field
+                  name="search"
+                  render={({ field }) => (
+                    <input
+                      {...field}
+                      type="text"
+                      placeholder="Search"
+                      className={styles['products-filter-search']}
+                      value={search}
+                      onChange={(event) => onChangeSearch(event, setFieldValue)}
+                    />
+                  )}
+                />
+                {/* Кнопка для закрытия поиска */}
+                {values.search.length > 0 && (
+                  <button
+                    className={styles['products-btn-clouse']}
+                    onClick={() => delTextInput(setFieldValue)}
+                    type="button"
+                  >
+                    <svg className={styles['products-svg-clouse']}>
+                      <use href={`${sprite}#Close`}></use>
+                    </svg>
+                  </button>
+                )}
+                {/* Кнопка для запуска поиска */}
+                <button
+                  className={styles['products-btn-search']}
+                  type="submit"
+                >
+                  <svg className={styles['products-svg-search']}>
+                    <use href={`${sprite}#Search`}></use>
+                  </svg>
+                </button>
+              </label>
+            </li>
 
-      {/* Выбор категории */}
-      <li>
-        <div className={styles['select-wrapper']}>
-          <Select
-            className={styles['select-wrapper']}
-            styles={customStyles}
-            value={category}
-            onChange={onCategoriesChange}
-            placeholder="Categories"
-            options={categoriesList || []}
-            theme={(theme) => ({
-              ...theme,
-              colors: {
-                ...theme.colors,
-                primary50: 'rgba(255, 255, 255, 0.10)',
-                primary: 'transparent',
-                neutral40: '#EFEDE8',
-                neutral20: 'transparent',
-                neutral30: 'transparent',
-                neutral50: 'rgba(239, 237, 232, 1)',
-                neutral80: 'rgba(239, 237, 232, 1)',
-              },
-            })}
-          />
-        </div>
-      </li>
+            {/* Выбор категории */}
+            <li>
+              <div className={styles['select-wrapper']}>
+                <Select
+                  className={styles['select-wrapper']}
+                  styles={customStyles}
+                  value={category}
+                  onChange={(event) => onCategoriesChange(event, setFieldValue)}
+                  placeholder="Categories"
+                  options={categoriesList || []}
+                  theme={(theme) => ({
+                    ...theme,
+                    colors: {
+                      ...theme.colors,
+                      primary50: 'rgba(255, 255, 255, 0.10)',
+                      primary: 'transparent',
+                      neutral40: '#EFEDE8',
+                      neutral20: 'transparent',
+                      neutral30: 'transparent',
+                      neutral50: 'rgba(239, 237, 232, 1)',
+                      neutral80: 'rgba(239, 237, 232, 1)',
+                    },
+                  })}
+                />
+              </div>
+            </li>
 
-      {/* Выбор рекомендаций */}
-      <li>
-        <div className={styles['select-wrapper']}>
-          <Select
-            className={styles['select-wrapper']}
-            onChange={onRecommendedChange}
-            styles={customStyles}
-            value={recommended}
-            theme={(theme) => ({
-              ...theme,
-              colors: {
-                ...theme.colors,
-                primary50: 'rgba(255, 255, 255, 0.10)',
-                primary: 'transparent',
-                neutral40: '#EFEDE8',
-                neutral20: 'transparent',
-                neutral30: 'transparent',
-                neutral50: 'rgba(239, 237, 232, 1)',
-                neutral80: 'rgba(239, 237, 232, 1)',
-              },
-            })}
-            options={optionsRec}
-          />
-        </div>
-      </li>
-    </ul>
+            {/* Выбор рекомендаций */}
+            <li>
+              <div className={styles['select-wrapper']}>
+                <Select
+                  className={styles['select-wrapper']}
+                  onChange={(event) =>
+                    onRecommendedChange(event, setFieldValue)
+                  }
+                  styles={customStyles}
+                  value={recommended}
+                  theme={(theme) => ({
+                    ...theme,
+                    colors: {
+                      ...theme.colors,
+                      primary50: 'rgba(255, 255, 255, 0.10)',
+                      primary: 'transparent',
+                      neutral40: '#EFEDE8',
+                      neutral20: 'transparent',
+                      neutral30: 'transparent',
+                      neutral50: 'rgba(239, 237, 232, 1)',
+                      neutral80: 'rgba(239, 237, 232, 1)',
+                    },
+                  })}
+                  options={optionsRec}
+                />
+              </div>
+            </li>
+          </ul>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
 export default ProductsFilter;
-
