a86f19b5d152026af7b8e848d04a34a84f4b31a1
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-20 19:53:20 +0100

    add

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Power_Pulse_TP">
           <App />
           <ToastContainer />
         </BrowserRouter>
