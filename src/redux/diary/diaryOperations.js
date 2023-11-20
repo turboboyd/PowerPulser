bf3880ed73718b9e9c3f8508846d458879a01d51
Rammbest <igor-kutsenko@ukr.net>
2023-11-20 14:30:04 +0200

    add notification message

--- src/redux/diary/diaryOperations.js
+++ src/redux/diary/diaryOperations.js
@@ -1,31 +1,35 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { instance } from '../services/instanceAPI';
-import { token } from '../services/tokenAPI';
-import { tokenState } from '../services/tokenState';
+import { toast } from "react-toastify";
+import notifyOptions from "../../utils/NotifyOptions";
+import "react-toastify/dist/ReactToastify.css";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { instance } from "../services/instanceAPI";
+import { token } from "../services/tokenAPI";
+import { tokenState } from "../services/tokenState";
 import {
   BACKEND_DIARY_EXERCISES_URL,
   BACKEND_DIARY_PRODUCTS_URL,
   BACKEND_DIARY_URL,
-} from '../../utils/const';
+} from "../../utils/const";
 
 export const fetchAllDiary = createAsyncThunk(
-  'diary/fetchAllDiary',
+  "diary/fetchAllDiary",
   async (params, thunkAPI) => {
     try {
       token.set(tokenState(thunkAPI));
       const paramsURL = Object.keys(params)
         .map((key) => `${key}=${params[key]}`)
-        .join('&');
+        .join("&");
       const { data } = await instance.get(`${BACKEND_DIARY_URL}?${paramsURL}`);
       return data;
     } catch (error) {
+      toast.error("Oops... Something went wrong! Try again!", notifyOptions);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const addProductDiary = createAsyncThunk(
-  'diary/addProductDiary',
+  "diary/addProductDiary",
   async (credentials, thunkAPI) => {
     // credentials: {
     //     date: "2023-11-13",
@@ -40,30 +44,37 @@ export const addProductDiary = createAsyncThunk(
         BACKEND_DIARY_PRODUCTS_URL,
         credentials
       );
+      toast.success("Product successfully added to diary!", notifyOptions);
       return data;
     } catch (error) {
+      toast.error("Oops... Something went wrong! Try again!", notifyOptions);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const deleteProductDiary = createAsyncThunk(
-  'diary/deleteProductDiary',
+  "diary/deleteProductDiary",
   async (idProduct, thunkAPI) => {
     try {
       token.set(tokenState(thunkAPI));
       const { data } = await instance.delete(
         `${BACKEND_DIARY_PRODUCTS_URL}/${idProduct}`
       );
+      toast.success(
+        "Product successfully deleteded from diary!",
+        notifyOptions
+      );
       return data;
     } catch (error) {
+      toast.error("Oops... Something went wrong! Try again!", notifyOptions);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const addExercisesDiary = createAsyncThunk(
-  'diary/addExercisesDiary',
+  "diary/addExercisesDiary",
   async (credentials, thunkAPI) => {
     // credentials: {
     //     date: "2023-11-13",
@@ -78,23 +89,28 @@ export const addExercisesDiary = createAsyncThunk(
         BACKEND_DIARY_EXERCISES_URL,
         credentials
       );
+      toast.success("Exercises successfully added!", notifyOptions);
       return data;
     } catch (error) {
+      toast.error("Oops... Something went wrong! Try again!", notifyOptions);
+
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const deleteExercisesDiary = createAsyncThunk(
-  'diary/deleteExercisesDiary',
+  "diary/deleteExercisesDiary",
   async (idExercises, thunkAPI) => {
     try {
       token.set(tokenState(thunkAPI));
       const { data } = await instance.delete(
         `${BACKEND_DIARY_EXERCISES_URL}/${idExercises}`
       );
+      toast.success("Exercises successfully deleteded!", notifyOptions);
       return data;
     } catch (error) {
+      toast.error("Oops... Something went wrong! Try again!", notifyOptions);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
