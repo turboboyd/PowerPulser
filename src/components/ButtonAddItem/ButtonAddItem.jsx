d14159cb80eeae4be483ace3cacd425615462418
dmytro-liashok <lyashokdima1@gmail.com>
2023-11-21 18:17:50 +0200

    add proptypes and fix diary page

--- src/components/ButtonAddItem/ButtonAddItem.jsx
+++ src/components/ButtonAddItem/ButtonAddItem.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import css from "./ButtonAddItem.module.css";
-import Icon from "../ComponIcon/Icon";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import css from './ButtonAddItem.module.css';
+
+import Icon from '../ComponIcon/Icon';
 
 const ButtonAddItem = ({ titleLink, titleRoute }) => {
   return (
     <>
-      <NavLink className={css.addButtonLink} to={titleRoute}>
-        <p className={css.addButtonTitle}>{titleLink}</p>
-        <Icon className={css.addButtonImg} iconId={"Arrow"} />
-      </NavLink>
+      <Link className={css.addButtonLink} to={titleRoute}>
+        {titleLink}
+        <Icon className={css.addButtonImg} iconId={'Arrow'} />
+      </Link>
     </>
   );
+};
+
+ButtonAddItem.propTypes = {
+  titleLink: PropTypes.string.isRequired,
 };
 
 export default ButtonAddItem;
--- src/components/ProductsTable/ProductsTable.jsx
+++ src/components/ProductsTable/ProductsTable.jsx
@@ -1,76 +1,85 @@
-import { useDispatch } from "react-redux";
-import Icon from "../ComponIcon/Icon";
-import css from "./ProductsTable.module.css";
-import { deleteProductDiary } from "../../redux/diary/diaryOperations";
+import { useDispatch } from 'react-redux';
+import Icon from '../ComponIcon/Icon';
+import css from './ProductsTable.module.css';
+import { deleteProductDiary } from '../../redux/diary/diaryOperations';
+import capitalizedWord from 'utils/capitalizedWord';
+import PropTypes from 'prop-types';
 
 const ProductsTable = ({ products }) => {
   const dispatch = useDispatch();
 
-  const deleteProduct = (_id) => {
-    dispatch(deleteProductDiary(_id));
+  const deleteProduct = id => {
+    dispatch(deleteProductDiary(id));
   };
 
   return (
     <ul className={css.diaryMainList}>
       {products.map(
-        ({_id, title, category, calories, amount, recommended }, i) => {
+        ({ _id, title, category, calories, amount, recommended }, i) => {
           return (
-          <li key={_id} className={css.diaryMainItem}>
-            <table className={css.table}>
-              <tbody className={css.bodyTable}>
-                <tr className={css.element}>
-                  <th className={css.nameCategory}>Title</th>
-                  <td className={css.nameValue}>{title}</td>
-                </tr>
-                <tr className={css.element}>
-                  <th className={css.nameCategory}>Category</th>
-                  <td className={css.nameValue}>{category}</td>
-                </tr>
-              </tbody>
-              <tbody className={css.elementWrap}>
-                <tr className={css.element}>
-                  <th className={css.nameCategory}>Calories</th>
-                  <td className={css.nameValue}>{calories}</td>
-                </tr>
-                <tr className={css.element}>
-                  <th className={css.nameCategory}>Weight</th>
-                  <td className={css.nameValue}>{amount}</td>
-                </tr>
-                <tr className={css.element}>
-                  <th className={css.nameCategory}>Recommend</th>
-                  <td className={css.nameValue}>
-                    <div className={`${css.recommendWrap}`}>
-                      <span
-                        className={
-                          recommended
-                            ? `${css.productRecommend}`
-                            : `${css.productNotRecommend}`
-                        }
-                      ></span>
-                      {recommended ? "Yes" : "No"}
-                    </div>
-                  </td>
-                </tr>
-                <tr className={css.element}>
-                  <th className={`${css.nameCategory} ${css.nameTrash}`}>
-                    Trash
-                  </th>
-                  <td
-                    className={css.trashValue}
-                    onClick={() => {
-                      deleteProduct(_id);
-                    }}
-                  >
-                    <Icon className={css.trashImg} iconId={"Trash"} />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
-        )}
+            <li key={_id} className={css.diaryMainItem}>
+              <table className={css.table}>
+                <tbody className={css.bodyTable}>
+                  <tr className={css.element}>
+                    <th className={css.nameCategory}>Title</th>
+                    <td className={css.nameValue}>{capitalizedWord(title)}</td>
+                  </tr>
+                  <tr className={css.element}>
+                    <th className={css.nameCategory}>Category</th>
+                    <td className={css.nameValue}>
+                      {capitalizedWord(category)}
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody className={css.elementWrap}>
+                  <tr className={css.element}>
+                    <th className={css.nameCategory}>Calories</th>
+                    <td className={css.nameValue}>{calories}</td>
+                  </tr>
+                  <tr className={css.element}>
+                    <th className={css.nameCategory}>Weight</th>
+                    <td className={css.nameValue}>{amount}</td>
+                  </tr>
+                  <tr className={css.element}>
+                    <th className={css.nameCategory}>Recommend</th>
+                    <td className={css.nameValue}>
+                      <div className={`${css.recommendWrap}`}>
+                        <span
+                          className={
+                            recommended
+                              ? `${css.productRecommend}`
+                              : `${css.productNotRecommend}`
+                          }
+                        ></span>
+                        {recommended ? 'Yes' : 'No'}
+                      </div>
+                    </td>
+                  </tr>
+                  <tr className={css.element}>
+                    <th className={`${css.nameCategory} ${css.nameTrash}`}>
+                      Trash
+                    </th>
+                    <td
+                      className={css.trashValue}
+                      onClick={() => {
+                        deleteProduct(_id);
+                      }}
+                    >
+                      <Icon className={css.trashImg} iconId={'Trash'} />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </li>
+          );
+        }
       )}
     </ul>
   );
+};
+
+ProductsTable.propTypes = {
+  products: PropTypes.array.isRequired,
 };
 
 export default ProductsTable;
