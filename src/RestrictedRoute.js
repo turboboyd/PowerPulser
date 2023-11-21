38bdfe92081c02c84a8e4c0c8e9088d27d0610f0
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-21 20:02:07 +0100

    add

--- src/RestrictedRoute.js
+++ src/RestrictedRoute.js
@@ -2,12 +2,13 @@ import { Navigate } from "react-router-dom";
 import useAuth from "./hooks/useAuth";
 import getLogoLink from "./utils/getLogoLink";
 
-const RestrictedRoute = ({ element,}) => {
+const RestrictedRoute = ({ children}) => {
+
   const { isVerify, user, isAuthCheck } = useAuth();
 
   const link = getLogoLink(isVerify, user);
   const shouldRedirect = isVerify && isAuthCheck;
-  return shouldRedirect ? <Navigate to={link} /> : element;
+  return shouldRedirect ? <Navigate to={link} /> : children;
 };
 
 export default RestrictedRoute;
